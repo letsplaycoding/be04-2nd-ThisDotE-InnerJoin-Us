593b8f52a2bf88280516212302a41a9ffb269f32
jhlee6515 <45275759+jhlee6515@users.noreply.github.com>
2024-03-07 12:00:09 +0900

    ModelMapper Bean 추가

--- innerjoinus-user-client-source/src/main/java/org/thisdote/innerjoinus/user/InnerjoinusUserClientSourceApplication.java
+++ innerjoinus-user-client-source/src/main/java/org/thisdote/innerjoinus/user/InnerjoinusUserClientSourceApplication.java
@@ -1,8 +1,10 @@
 package org.thisdote.innerjoinus.user;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,11 @@ public class InnerjoinusUserClientSourceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InnerjoinusUserClientSourceApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
     }
 
 }
