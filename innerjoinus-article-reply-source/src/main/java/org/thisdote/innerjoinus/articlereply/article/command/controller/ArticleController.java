d47833f83a6d834b4bf8b9d38f74cb50208fb1e1
shindaeyeong <eodud3196@gmail.com>
2024-03-07 20:41:19 +0900

    Modify Article
    게시글 제목, 게시글 내용 Post 입력 후 수정

--- innerjoinus-article-reply-source/src/main/java/org/thisdote/innerjoinus/articlereply/article/command/controller/ArticleController.java
+++ innerjoinus-article-reply-source/src/main/java/org/thisdote/innerjoinus/articlereply/article/command/controller/ArticleController.java
@@ -2,12 +2,12 @@ package org.thisdote.innerjoinus.articlereply.article.command.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.thisdote.innerjoinus.articlereply.article.command.service.CommandArticleService;
-import org.thisdote.innerjoinus.articlereply.article.command.vo.RequestDeleteArticle;
-import org.thisdote.innerjoinus.articlereply.article.command.vo.ResponseDeleteArticle;
+import org.thisdote.innerjoinus.articlereply.article.command.vo.*;
 import org.thisdote.innerjoinus.articlereply.article.dto.ArticleDTO;
 
 @Slf4j
@@ -22,6 +22,21 @@ public class ArticleController {
         this.commandArticleService = commandArticleService;
     }
 
+    @GetMapping("/regist_article")
+    public void registArticle(){}
+
+    @PostMapping("/regist_article")
+    public ResponseEntity<ResponseRegistArticle> registArticle(@RequestBody RequestRegistArticle registArticle){
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        ArticleDTO articleDTO = modelMapper.map(registArticle, ArticleDTO.class);
+        commandArticleService.registArticle(articleDTO);
+
+        ResponseRegistArticle responseRegistArticle = new ResponseRegistArticle();
+        responseRegistArticle.setMessage(articleDTO.getArticleTitle());
+        System.out.println("responseRegistArticle = " + responseRegistArticle);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(responseRegistArticle);
+    }
+
     @GetMapping("/delete_article")
     public void deleteArticle(){}
 
@@ -34,5 +49,16 @@ public class ArticleController {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseDeleteArticle);
     }
 
+    @GetMapping("/modify_article")
+    public void modifyArticle(){}
 
+    @PostMapping("/modify_article")
+    public ResponseEntity<ResponseModifyArticle> modifyArticle(@RequestBody RequestModifyArticle modifyArticle){
+        ArticleDTO articleDTO = modelMapper.map(modifyArticle, ArticleDTO.class);
+        commandArticleService.modifyArticle(articleDTO);
+
+        ResponseModifyArticle responseModifyArticle = new  ResponseModifyArticle();
+        responseModifyArticle.setMessage("수정 완료");
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(responseModifyArticle);
+    }
 }
--- innerjoinus-article-reply-source/src/main/java/org/thisdote/innerjoinus/articlereply/article/command/service/CommandArticleService.java
+++ innerjoinus-article-reply-source/src/main/java/org/thisdote/innerjoinus/articlereply/article/command/service/CommandArticleService.java
@@ -4,4 +4,8 @@ import org.thisdote.innerjoinus.articlereply.article.dto.ArticleDTO;
 
 public interface CommandArticleService {
     String deleteArticle(ArticleDTO articleDTO);
+
+    void modifyArticle(ArticleDTO modifyArticle);
+
+    void registArticle(ArticleDTO articleDTO);
 }
