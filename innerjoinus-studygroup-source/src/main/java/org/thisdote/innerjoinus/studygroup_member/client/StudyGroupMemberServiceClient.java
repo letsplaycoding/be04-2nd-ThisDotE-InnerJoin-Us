386d640252fff7b7ec09306efdaf107bd3f789db
Bodrami <dlwldms1523@gmail.com>
2024-03-08 18:26:38 +0900

    FeignClient 작성을 위해 main()에 어노테이션 붙임

--- innerjoinus-studygroup-source/src/main/java/org/thisdote/innerjoinus/studygroup_member/client/StudyGroupMemberServiceClient.java
+++ innerjoinus-studygroup-source/src/main/java/org/thisdote/innerjoinus/studygroup_member/client/StudyGroupMemberServiceClient.java
@@ -1,8 +1,16 @@
 package org.thisdote.innerjoinus.studygroup_member.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.thisdote.innerjoinus.studygroup_member.query.vo.ResponseStudyGroupMember;
 
-@FeignClient(name = "swcamp-studygroup_member", url = "localhost:8080")
+import java.util.List;
+
+@FeignClient(name = "innerjoinus-studygroup-member", url = "localhost:8080")
 public class StudyGroupMemberServiceClient {
+
+//    @GetMapping("/studygroup-member/studygroup_member/")
+//    List<ResponseStudyGroupMember> getStudyGroupMember (@PathVariable("studyGroupMemberId") String studyGroupMemberId)
 
 }
