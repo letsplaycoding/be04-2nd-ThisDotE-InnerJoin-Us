ecb07479cd75856cd1cbf9f5c6ea39fda085ac83
jhlee6515 <45275759+jhlee6515@users.noreply.github.com>
2024-03-08 12:45:27 +0900

    controller requestMapping 수정
    "/users" -> "/"

--- innerjoinus-user-client-source/src/main/java/org/thisdote/innerjoinus/user/command/controller/UserCommandController.java
+++ innerjoinus-user-client-source/src/main/java/org/thisdote/innerjoinus/user/command/controller/UserCommandController.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/")
 public class UserCommandController {
 
     private final ModelMapper mapper;
--- innerjoinus-user-client-source/src/main/java/org/thisdote/innerjoinus/user/query/controller/UserQueryController.java
+++ innerjoinus-user-client-source/src/main/java/org/thisdote/innerjoinus/user/query/controller/UserQueryController.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/")
 public class UserQueryController {
 
     private final ModelMapper mapper;
