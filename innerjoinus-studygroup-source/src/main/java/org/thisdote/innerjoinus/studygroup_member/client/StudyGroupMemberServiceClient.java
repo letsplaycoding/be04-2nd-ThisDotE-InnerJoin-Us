e3ed3ece1325504fa8501b26debe8ac61b8ac622
alu98@naver.com <alu98@naver.com>
2024-03-08 19:00:54 +0900

    feign-studygroup에 Init

--- innerjoinus-studygroup-source/src/main/java/org/thisdote/innerjoinus/studygroup_member/client/StudyGroupMemberServiceClient.java
+++ innerjoinus-studygroup-source/src/main/java/org/thisdote/innerjoinus/studygroup_member/client/StudyGroupMemberServiceClient.java
@@ -2,7 +2,7 @@ package org.thisdote.innerjoinus.studygroup_member.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "swcamp-studygroup_member", url = "localhost:8080")
+@FeignClient(name = "swcamp-studygroup-member", url = "localhost:8080")
 public class StudyGroupMemberServiceClient {
 
 }
