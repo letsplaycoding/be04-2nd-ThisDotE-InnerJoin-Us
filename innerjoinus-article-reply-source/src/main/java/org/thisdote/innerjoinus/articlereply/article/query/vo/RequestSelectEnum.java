462e24082176acf03dc89e2605a59a3546a98a6f
shindaeyeong <eodud3196@gmail.com>
2024-03-08 15:03:37 +0900

    Criteria
    Criteria enum 이용한 다이나믹 쿼리 조회 적용 완료.

---
+++ innerjoinus-article-reply-source/src/main/java/org/thisdote/innerjoinus/articlereply/article/query/vo/RequestSelectEnum.java
@@ -0,0 +1,9 @@
+package org.thisdote.innerjoinus.articlereply.article.query.vo;
+
+import lombok.Data;
+
+@Data
+public class RequestSelectEnum {
+    private String criteriaEnum;
+    private Object select;
+}
