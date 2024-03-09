ddd935a6df02ce8e49ebf50f3ec1740446f60362
shindaeyeong <eodud3196@gmail.com>
2024-03-09 10:59:56 +0900

    Response, client
    userClient, data를 받아올 VO생성

--- innerjoinus-article-reply-source/src/main/java/org/thisdote/innerjoinus/articlereply/client/UserClient.java
+++ innerjoinus-article-reply-source/src/main/java/org/thisdote/innerjoinus/articlereply/client/UserClient.java
@@ -2,8 +2,12 @@ package org.thisdote.innerjoinus.articlereply.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thisdote.innerjoinus.articlereply.article.command.vo.ResponseUser;
+
+import java.util.List;
 
 @FeignClient(name = "INNERJOINUS-USER", url = "localhost:8000")
-public class UserClient {
-    @GetMapping()
+public interface UserClient {
+    @GetMapping("/user/users/all")
+    List<ResponseUser> getAllUser();
 }
